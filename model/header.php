6067afee9759a3cd7624614c4e92dd4b6e6f90da
Fabrice DUJARDIN <fdujardin@giep.nc>
2022-03-09 08:47:45 +1100

    update session time

--- model/header.php
+++ model/header.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-<?php session_start();?>
+<?php
+session_start([
+    'cookie_lifetime' => 604800,
+]);?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
